a2fe8e6550ca7000fdc6b5baff451c1a8b66642f
Maciej Torhan <mtorhan@antmicro.com>
2023-03-14 12:21:33 +0000

    [#42332] build_tools: run_simulation: Add script for running simulation
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

---
+++ build_tools/run_simulation.sh
@@ -0,0 +1,11 @@
+ROOTDIR=$(dirname $(dirname $(realpath $0)))
+
+command="start;"
+if [[ "$1" == "debug" ]]; then
+    command="machine StartGdbServer 3333;"
+fi
+
+bin_file=$(realpath build/build-riscv/iree-runtime/iree_runtime)
+(cd "${ROOTDIR}" && ./build/renode/renode -e "\$bin=@${bin_file}; i @sim/config/springbok.resc; \
+${command} sysbus.vec_controlblock WriteDoubleWord 0xc 0" \
+    --disable-xwt --console)
