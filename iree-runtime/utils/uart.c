ace3a3d5f531bc2445b6c0aa50d727faa58124b5
Maciej Torhan <mtorhan@antmicro.com>
2023-03-17 13:45:11 +0000

    [#42332] iree-runtime: utils: uart: Fix code format
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- iree-runtime/utils/uart.c
+++ iree-runtime/utils/uart.c
@@ -6,30 +6,32 @@ UART_STATUS uart_init(const uart_config *config)
 {
     if (g_uart.initialized)
     {
-        return UART_OK;
+        return UART_STATUS_OK;
     }
 
     g_uart.registers = (uart_registers *)UART_ADDRESS;
 
     if (config->data_bits < 5u || config->data_bits > 8u)
     {
-        return UART_INVALID_ARGUMENT_WORDSIZE;
+        return UART_STATUS_INVALID_ARGUMENT_WORDSIZE;
     }
     if (config->stop_bits == 0u || config->stop_bits > 2u)
     {
-        return UART_INVALID_ARGUMENT_STOP_BITS;
+        return UART_STATUS_INVALID_ARGUMENT_STOP_BITS;
     }
     if (config->baudrate < 110u || config->baudrate > 460800u)
     {
-        return UART_INVALID_ARGUMENT_BAUDRATE;
+        return UART_STATUS_INVALID_ARGUMENT_BAUDRATE;
     }
 
     g_uart.registers->CR &= ~CR_UARTEN;
     while (g_uart.registers->FR & FR_BUSY)
-        ;
+    {
+    }
     g_uart.registers->LCRH &= ~LCRH_FEN;
 
-    double intpart, fractpart;
+    double intpart = 0;
+    double fractpart = 0;
     double baudrate_divisor = (double)REF_CLOCK / (16u * config->baudrate);
     fractpart = modf(baudrate_divisor, &intpart);
 
@@ -84,13 +86,14 @@ UART_STATUS uart_init(const uart_config *config)
 
     g_uart.initialized = true;
 
-    return UART_OK;
+    return UART_STATUS_OK;
 }
 
 void uart_putchar(uint8_t c)
 {
     while (g_uart.registers->FR & FR_TXFF)
-        ;
+    {
+    }
     g_uart.registers->DR = c;
 }
 
@@ -106,16 +109,16 @@ UART_STATUS uart_getchar(uint8_t *c)
 {
     if (g_uart.registers->FR & FR_RXFE)
     {
-        return UART_NO_DATA;
+        return UART_STATUS_NO_DATA;
     }
 
     *c = g_uart.registers->DR & DR_DATA_MASK;
     if (g_uart.registers->RSRECR & RSRECR_ERR_MASK)
     {
         g_uart.registers->RSRECR &= RSRECR_ERR_MASK;
-        return UART_RECEIVE_ERROR;
+        return UART_STATUS_RECEIVE_ERROR;
     }
-    return UART_OK;
+    return UART_STATUS_OK;
 }
 
 UART_STATUS uart_read(uint8_t *data, size_t data_length)
@@ -127,23 +130,23 @@ UART_STATUS uart_read(uint8_t *data, size_t data_length)
     CSR_READ(start_timer, CSR_TIME);
     while (i < data_length)
     {
-        uint8_t c;
+        uint8_t c = 0;
         UART_STATUS status = uart_getchar(&c);
-        if (UART_OK == status)
+        if (UART_STATUS_OK == status)
         {
             CSR_READ(start_timer, CSR_TIME);
             data[i] = c;
             ++i;
         }
-        else if (UART_RECEIVE_ERROR == status)
+        else if (UART_STATUS_RECEIVE_ERROR == status)
         {
-            return UART_RECEIVE_ERROR;
+            return UART_STATUS_RECEIVE_ERROR;
         }
         CSR_READ(end_timer, CSR_TIME);
-        if (end_timer - start_timer > UART_TIMEOUT_S * REF_CLOCK)
+        if (end_timer - start_timer > UART_TIMEOUT_S * TIMER_CLOCK_FREQ)
         {
-            return UART_TIMEOUT;
+            return UART_STATUS_TIMEOUT;
         }
     }
-    return UART_OK;
+    return UART_STATUS_OK;
 }
