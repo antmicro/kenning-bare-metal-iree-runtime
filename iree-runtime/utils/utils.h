6bd1c9e4710103dc66dcd640dd32821a59212419
Maciej Torhan <mtorhan@antmicro.com>
2023-03-17 13:45:47 +0000

    [#42332] iree-runtime: utils: utils: Fix code format, add timer clock freq
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- iree-runtime/utils/utils.h
+++ iree-runtime/utils/utils.h
@@ -1,16 +1,18 @@
 #ifndef IREE_RUNTIME_UTILS_UTILS_H_
 #define IREE_RUNTIME_UTILS_UTILS_H_
 
-#define RETURN_ON_ERROR(status, err_code)                                                                              \
-    if (status)                                                                                                        \
-    {                                                                                                                  \
-        return err_code;                                                                                               \
+#define RETURN_ON_ERROR(status, err_code) \
+    if (status)                           \
+    {                                     \
+        return err_code;                  \
     }
 
 #define CSR_CYCLE (0xC00)
 #define CSR_TIME (0xC01)
 
 #define CSR_READ(v, csr) __asm__ __volatile__("csrr %0, %1" : "=r"(v) : "n"(csr) : /* clobbers: none */);
+
+#define TIMER_CLOCK_FREQ (24000000u) /* 24 MHz */
 
 #define GENERATE_ENUM(ENUM) ENUM,
 #define GENERATE_STR(STR) #STR,
