64bd912c470416c788b5d9b0fdb0c1c7179b65bd
Maciej Torhan <mtorhan@antmicro.com>
2023-03-24 15:33:13 +0100

    [#42924] iree-runtime: utils: utils: Add csr read mock and macro for pointer validation
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- iree-runtime/utils/utils.h
+++ iree-runtime/utils/utils.h
@@ -1,6 +1,8 @@
 #ifndef IREE_RUNTIME_UTILS_UTILS_H_
 #define IREE_RUNTIME_UTILS_UTILS_H_
 
+#define IS_VALID_POINTER(ptr) (NULL != ptr)
+
 #define RETURN_ON_ERROR(status, err_code) \
     if (status)                           \
     {                                     \
@@ -10,7 +12,18 @@
 #define CSR_CYCLE (0xC00)
 #define CSR_TIME (0xC01)
 
+#ifndef __UNIT_TEST__
 #define CSR_READ(v, csr) __asm__ __volatile__("csrr %0, %1" : "=r"(v) : "n"(csr) : /* clobbers: none */);
+#else // __UNIT_TEST__
+#define CSR_READ(v, csr)                      \
+    do                                        \
+    {                                         \
+        extern uint32_t mock_csr;             \
+        extern void mock_csr_read_callback(); \
+        v = mock_csr;                         \
+        mock_csr_read_callback();             \
+    } while (0);
+#endif // __UNIT_TEST__
 
 #define TIMER_CLOCK_FREQ (24000000u) /* 24 MHz */
 
