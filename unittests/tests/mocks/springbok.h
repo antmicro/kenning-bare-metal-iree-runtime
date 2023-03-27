9a363bdcc6229eb73e0e9684f9ef0326f5303130
Maciej Torhan <mtorhan@antmicro.com>
2023-03-27 16:02:58 +0200

    [#42924] unittests: tests: mocks: springbok: Add springbok mock
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

---
+++ unittests/tests/mocks/springbok.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#define LOG_ERROR(msg, args...) printf("[ERROR] " msg, ##args);
+#define LOG_WARN(msg, args...) printf("[WARN] " msg, ##args);
+#define LOG_INFO(msg, args...) printf("[INFO] " msg, ##args);
+#define LOG_DEBUG(msg, args...) printf("[DEBUG] " msg, ##args);
+#define LOG_NOISY(msg, args...) printf("[NOISY] " msg, ##args);
