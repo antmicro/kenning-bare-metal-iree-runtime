597d2f776dd15754d5820844724d7b912c3d3d01
Maciej Torhan <mtorhan@antmicro.com>
2023-03-28 10:36:25 +0200

    [#42924] unittests: tests: mocks: iree_wrapper: Refactor get stats
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- unittests/tests/mocks/iree_wrapper.h
+++ unittests/tests/mocks/iree_wrapper.h
@@ -81,8 +81,9 @@ IREE_WRAPPER_STATUS create_context(const uint8_t *model_data, const size_t model
 IREE_WRAPPER_STATUS prepare_input_buffer(const MlModel *model_struct, const uint8_t *model_input);
 IREE_WRAPPER_STATUS prepare_output_buffer();
 IREE_WRAPPER_STATUS run_inference();
-IREE_WRAPPER_STATUS get_output();
-void get_model_stats(uint8_t *statistics, size_t *statistics_size);
+IREE_WRAPPER_STATUS get_output(uint8_t *model_output);
+IREE_WRAPPER_STATUS get_model_stats(const size_t statistics_buffer_size, uint8_t *statistics_buffer,
+                                    size_t *statistics_size);
 void release_input_buffer();
 void release_output_buffer();
 
