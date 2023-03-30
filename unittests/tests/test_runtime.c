23d97a03eac70b8a9a0a072e6f26008608113aa4
Maciej Torhan <mtorhan@antmicro.com>
2023-03-30 14:39:48 +0200

    [#42924] unittests: tests: test_runtime: Add docs to tests
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- unittests/tests/test_runtime.c
+++ unittests/tests/test_runtime.c
@@ -12,6 +12,13 @@ const char *const MODEL_STATUS_STR[] = {MODEL_STATUSES(GENERATE_STR)};
 const char *const MESSAGE_TYPE_STR[] = {MESSAGE_TYPES(GENERATE_STR)};
 const char *const SERVER_STATUS_STR[] = {SERVER_STATUSES(GENERATE_STR)};
 
+/**
+ * Prepares message of given type and payload and store result in g_message
+ *
+ * @param msg_type type of the message
+ * @param payload payload of the message
+ * @param payload_size size of the payload
+ */
 void prepare_message(message_type_t msg_type, uint8_t *payload, size_t payload_size);
 
 void setUp(void) {}
@@ -28,6 +35,10 @@ void tearDown(void)
 // ========================================================
 // ok_callback
 // ========================================================
+
+/**
+ * Tests if ok callback has no response
+ */
 void test_RuntimeOKCallbackShouldReturnNoReponse(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -40,6 +51,9 @@ void test_RuntimeOKCallbackShouldReturnNoReponse(void)
     TEST_ASSERT_EQUAL_PTR(NULL, g_message);
 }
 
+/**
+ * Tests if ok callback fails for invalid pointer
+ */
 void test_RuntimeOKCallbackShouldFailForInvalidPointer(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -56,6 +70,9 @@ TEST_CASE(MESSAGE_TYPE_PROCESS)
 TEST_CASE(MESSAGE_TYPE_OUTPUT)
 TEST_CASE(MESSAGE_TYPE_STATS)
 TEST_CASE(MESSAGE_TYPE_IOSPEC)
+/**
+ * Tests if ok callback fails for request message type
+ */
 void test_RuntimeOkCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE message_type)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -70,6 +87,10 @@ void test_RuntimeOkCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE message_
 // ========================================================
 // error_callback
 // ========================================================
+
+/**
+ * Tests if error callback has no response
+ */
 void test_RuntimeErrorCallbackShouldReturnNoReponse(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -82,6 +103,9 @@ void test_RuntimeErrorCallbackShouldReturnNoReponse(void)
     TEST_ASSERT_EQUAL_PTR(NULL, g_message);
 }
 
+/**
+ * Tests if error callback fails for invalid pointer
+ */
 void test_RuntimeErrorCallbackShouldFailForInvalidPointer(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -98,6 +122,9 @@ TEST_CASE(MESSAGE_TYPE_PROCESS)
 TEST_CASE(MESSAGE_TYPE_OUTPUT)
 TEST_CASE(MESSAGE_TYPE_STATS)
 TEST_CASE(MESSAGE_TYPE_IOSPEC)
+/**
+ * Tests if error callback fails for request message type
+ */
 void test_RuntimeErrorCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE message_type)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -112,6 +139,10 @@ void test_RuntimeErrorCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE messa
 // ========================================================
 // data_callback
 // ========================================================
+
+/**
+ * Tests if data callback properly loads model input
+ */
 void test_RuntimeDataCallbackShouldLoadDataToModelInput(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -128,6 +159,9 @@ void test_RuntimeDataCallbackShouldLoadDataToModelInput(void)
     TEST_ASSERT_EQUAL_UINT(RUNTIME_STATUS_OK, runtime_status);
 }
 
+/**
+ * Tests if data callback fails if model input loading fails
+ */
 void test_RuntimeDataCallbackShouldFailIfLoadModelInputFails(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -144,6 +178,9 @@ void test_RuntimeDataCallbackShouldFailIfLoadModelInputFails(void)
     TEST_ASSERT_EQUAL_UINT(RUNTIME_STATUS_MODEL_ERROR, runtime_status);
 }
 
+/**
+ * Tests if data callback fails for invalid pointer
+ */
 void test_RuntimeDataCallbackShouldFailForInvalidPointer(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -160,6 +197,9 @@ TEST_CASE(MESSAGE_TYPE_PROCESS)
 TEST_CASE(MESSAGE_TYPE_OUTPUT)
 TEST_CASE(MESSAGE_TYPE_STATS)
 TEST_CASE(MESSAGE_TYPE_IOSPEC)
+/**
+ * Tests if data callback fails for request message type
+ */
 void test_RuntimeDataCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE message_type)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -174,6 +214,10 @@ void test_RuntimeDataCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE messag
 // ========================================================
 // model_callback
 // ========================================================
+
+/**
+ * Tests if model callback properly loads model weights
+ */
 void test_RuntimeModelCallbackShouldLoadModelWeights(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -190,6 +234,9 @@ void test_RuntimeModelCallbackShouldLoadModelWeights(void)
     TEST_ASSERT_EQUAL_UINT(RUNTIME_STATUS_OK, runtime_status);
 }
 
+/**
+ * Tests if model callback fails if model weights loading fails
+ */
 void test_RuntimeModelCallbackShouldFailIfLoadModelWeightsFails(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -206,6 +253,9 @@ void test_RuntimeModelCallbackShouldFailIfLoadModelWeightsFails(void)
     TEST_ASSERT_EQUAL_UINT(RUNTIME_STATUS_MODEL_ERROR, runtime_status);
 }
 
+/**
+ * Tests if model callback fails for invalid pointer
+ */
 void test_RuntimeModelCallbackShouldFailForInvalidPointer(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -222,6 +272,9 @@ TEST_CASE(MESSAGE_TYPE_PROCESS)
 TEST_CASE(MESSAGE_TYPE_OUTPUT)
 TEST_CASE(MESSAGE_TYPE_STATS)
 TEST_CASE(MESSAGE_TYPE_IOSPEC)
+/**
+ * Tests if model callback fails for request message type
+ */
 void test_RuntimeModelCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE message_type)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -236,6 +289,10 @@ void test_RuntimeModelCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE messa
 // ========================================================
 // process_callback
 // ========================================================
+
+/**
+ * Tests if process callback properly runs model inference
+ */
 void test_RuntimeProcessCallbackShouldRunModel(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -250,6 +307,9 @@ void test_RuntimeProcessCallbackShouldRunModel(void)
     TEST_ASSERT_EQUAL_UINT(RUNTIME_STATUS_OK, runtime_status);
 }
 
+/**
+ * Tests if process callback fails if model inference fails
+ */
 void test_RuntimeProcessCallbackShouldFailIfRunModelFails(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -264,6 +324,9 @@ void test_RuntimeProcessCallbackShouldFailIfRunModelFails(void)
     TEST_ASSERT_EQUAL_UINT(RUNTIME_STATUS_MODEL_ERROR, runtime_status);
 }
 
+/**
+ * Tests if process callback fails for invalid pointer
+ */
 void test_RuntimeProcessCallbackShouldFailForInvalidPointer(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -280,6 +343,9 @@ TEST_CASE(MESSAGE_TYPE_MODEL)
 TEST_CASE(MESSAGE_TYPE_OUTPUT)
 TEST_CASE(MESSAGE_TYPE_STATS)
 TEST_CASE(MESSAGE_TYPE_IOSPEC)
+/**
+ * Tests if process callback fails for request message type
+ */
 void test_RuntimeProcessCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE message_type)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -294,7 +360,11 @@ void test_RuntimeProcessCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE mes
 // ========================================================
 // output_callback
 // ========================================================
-void test_RuntimeOutputCallbackShouldRunModel(void)
+
+/**
+ * Tests if output callback properly loads model output
+ */
+void test_RuntimeOutputCallbackShouldLoadModelOutput(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
 
@@ -308,6 +378,9 @@ void test_RuntimeOutputCallbackShouldRunModel(void)
     TEST_ASSERT_EQUAL_UINT(RUNTIME_STATUS_OK, runtime_status);
 }
 
+/**
+ * Tests if output callback fails when model output loading fails
+ */
 void test_RuntimeOutputCallbackShouldFailIfGetModelOutputFails(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -322,6 +395,9 @@ void test_RuntimeOutputCallbackShouldFailIfGetModelOutputFails(void)
     TEST_ASSERT_EQUAL_UINT(RUNTIME_STATUS_MODEL_ERROR, runtime_status);
 }
 
+/**
+ * Tests if output callback fails for invalid pointer
+ */
 void test_RuntimeOutputCallbackShouldFailForInvalidPointer(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -338,6 +414,9 @@ TEST_CASE(MESSAGE_TYPE_MODEL)
 TEST_CASE(MESSAGE_TYPE_PROCESS)
 TEST_CASE(MESSAGE_TYPE_STATS)
 TEST_CASE(MESSAGE_TYPE_IOSPEC)
+/**
+ * Tests if output callback fails for request message type
+ */
 void test_RuntimeOutputCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE message_type)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -352,7 +431,11 @@ void test_RuntimeOutputCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE mess
 // ========================================================
 // stats_callback
 // ========================================================
-void test_RuntimeStatsCallbackShouldRunModel(void)
+
+/**
+ * Tests if IO spec callback properly loads execution statistics
+ */
+void test_RuntimeStatsCallbackShouldLoadStats(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
 
@@ -366,6 +449,9 @@ void test_RuntimeStatsCallbackShouldRunModel(void)
     TEST_ASSERT_EQUAL_UINT(RUNTIME_STATUS_OK, runtime_status);
 }
 
+/**
+ * Tests if IO spec callback fails if get statistics fails
+ */
 void test_RuntimeStatsCallbackShouldFailIfGetModelOutputFails(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -381,6 +467,9 @@ void test_RuntimeStatsCallbackShouldFailIfGetModelOutputFails(void)
     TEST_ASSERT_EQUAL_UINT(RUNTIME_STATUS_MODEL_ERROR, runtime_status);
 }
 
+/**
+ * Tests if stats callback fails for invalid pointer
+ */
 void test_RuntimeStatsCallbackShouldFailForInvalidPointer(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -397,6 +486,9 @@ TEST_CASE(MESSAGE_TYPE_MODEL)
 TEST_CASE(MESSAGE_TYPE_PROCESS)
 TEST_CASE(MESSAGE_TYPE_OUTPUT)
 TEST_CASE(MESSAGE_TYPE_IOSPEC)
+/**
+ * Tests if stats callback fails for request message type
+ */
 void test_RuntimeStatsCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE message_type)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -443,6 +535,9 @@ void test_RuntimeIOSpecCallbackShouldFailIfGetModelOutputFails(void)
     TEST_ASSERT_EQUAL_UINT(RUNTIME_STATUS_MODEL_ERROR, runtime_status);
 }
 
+/**
+ * Tests if IO spec callback fails for invalid pointer
+ */
 void test_RuntimeIOSpecCallbackShouldFailForInvalidPointer(void)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -459,6 +554,9 @@ TEST_CASE(MESSAGE_TYPE_MODEL)
 TEST_CASE(MESSAGE_TYPE_PROCESS)
 TEST_CASE(MESSAGE_TYPE_OUTPUT)
 TEST_CASE(MESSAGE_TYPE_STATS)
+/**
+ * Tests if IO spec callback fails for request message type
+ */
 void test_RuntimeIOSpecCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE message_type)
 {
     RUNTIME_STATUS runtime_status = RUNTIME_STATUS_OK;
@@ -473,6 +571,7 @@ void test_RuntimeIOSpecCallbackShouldFailForInvalidMessageType(MESSAGE_TYPE mess
 // ========================================================
 // helper functions
 // ========================================================
+
 void prepare_message(message_type_t msg_type, uint8_t *payload, size_t payload_size)
 {
     if (IS_VALID_POINTER(g_message))
