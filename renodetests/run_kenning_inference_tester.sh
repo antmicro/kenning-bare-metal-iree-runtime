b004193a185e44c0909224b337d3ac6bcecc5780
Maciej Torhan <mtorhan@antmicro.com>
2023-04-12 14:08:25 +0200

    [#43218] renodetests: run_kenning_inference_tester: Update Kenning inference tester script
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- renodetests/run_kenning_inference_tester.sh
+++ renodetests/run_kenning_inference_tester.sh
@@ -1,3 +1,2 @@
 cd third-party/kenning
-git checkout 42337-uart-protocol-for-iree-runtime
 python3 -m kenning.scenarios.json_inference_tester ./scripts/jsonconfigs/iree-bare-metal-inference.json ./output.json
