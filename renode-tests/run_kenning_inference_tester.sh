59fbd832cec89bc95da838a0cb7834fea537458d
Maciej Torhan <mtorhan@antmicro.com>
2023-04-13 11:12:01 +0200

    [#43218] renode-tests: run_kenning_inference_tester: Change output file location
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- renode-tests/run_kenning_inference_tester.sh
+++ renode-tests/run_kenning_inference_tester.sh
@@ -1,2 +1,9 @@
-cd third-party/kenning
-python3 -m kenning.scenarios.json_inference_tester ./scripts/jsonconfigs/iree-bare-metal-inference.json ./output.json
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_DIR=$(dirname $SCRIPT_DIR)
+
+cd $PROJECT_DIR/third-party/kenning
+python3 -m kenning.scenarios.json_inference_tester \
+    ./scripts/jsonconfigs/iree-bare-metal-inference.json \
+    $SCRIPT_DIR/results/kenning_output.json
