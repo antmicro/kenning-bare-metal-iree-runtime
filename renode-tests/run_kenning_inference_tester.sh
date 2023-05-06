c1d867dc123109331f2217558924314b614b96b9
Grzegorz Latosinski <glatosinski@antmicro.com>
2023-05-07 00:27:08 +0200

    [#44177] renode-tests: run_kenning_inference_tester.sh: Added setting proper compiler for IREE
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- renode-tests/run_kenning_inference_tester.sh
+++ renode-tests/run_kenning_inference_tester.sh
@@ -7,6 +7,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR=$(dirname $SCRIPT_DIR)
 
+export LD_LIBRARY_PATH=${PROJECT_DIR}/build/iree_compiler/lib/:$LD_LIBRARY_PATH
+export PATH=${PROJECT_DIR}/build/iree_compiler/bin:$PATH
+export IREE_TOOL_PATH=${PROJECT_DIR}/build/iree_compiler/bin
+
 cd $PROJECT_DIR/third-party/kenning
 python3 -m kenning.scenarios.json_inference_tester \
     ./scripts/jsonconfigs/iree-bare-metal-inference.json \
