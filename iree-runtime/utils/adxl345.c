b80fcfb103a6cdb3cd7c32b1ab6edd596f9da5dd
Maciej Torhan <mtorhan@antmicro.com>
2023-06-07 15:56:43 +0200

    [#45270] iree-runtime: utils: adxl345: Add data preprocessing, add sensor address as define
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- iree-runtime/utils/adxl345.c
+++ iree-runtime/utils/adxl345.c
@@ -6,17 +6,27 @@
 
 #include "adxl345.h"
 
-ERROR_STATUS adxl345_read_data(int16_t *x, int16_t *y, int16_t *z)
+GENERATE_MODULE_STATUSES_STR(ADXL345);
+
+status_t adxl345_read_data(adxl345_data_t *data)
 {
-    I2C_STATUS i2c_status = I2C_STATUS_OK;
-    uint8_t data[6];
+    status_t status = STATUS_OK;
 
-    i2c_status = i2c_read_target_registers(ADXL345_ADDRESS, ADXL345_DATA_X0, 6, data);
-    RETURN_ON_ERROR(i2c_status, ERROR_STATUS_ERROR);
+    VALIDATE_POINTER(data, ADXL345_STATUS_INV_PTR);
 
-    *x = data[0] | (data[1] << 8);
-    *y = data[2] | (data[3] << 8);
-    *z = data[4] | (data[5] << 8);
+    struct
+    {
+        int16_t x;
+        int16_t y;
+        int16_t z;
+    } raw_data;
 
-    return ERROR_STATUS_OK;
+    status = i2c_read_target_registers(ADXL345_I2C_ADDRESS, ADXL345_DATA_X0, sizeof(raw_data), (uint8_t *)&raw_data);
+    RETURN_ON_ERROR(status, status);
+
+    data->x = 4 * (float)raw_data.x;
+    data->y = 4 * (float)raw_data.y;
+    data->z = 4 * (float)raw_data.z;
+
+    return STATUS_OK;
 }
--- iree-runtime/utils/adxl345.h
+++ iree-runtime/utils/adxl345.h
@@ -7,16 +7,46 @@
 #ifndef IREE_RUNTIME_UTILS_ADXL345_H_
 #define IREE_RUNTIME_UTILS_ADXL345_H_
 
-#include "springbok.h"
-
 #include "i2c.h"
+#include "utils.h"
 
-#define ADXL345_ADDRESS (0x80)
+/**
+ * ADXL345 custom error codes
+ */
+#define ADXL345_STATUSES(STATUS)
+
+GENERATE_MODULE_STATUSES(ADXL345);
+
+#define ADXL345_I2C_ADDRESS (0x1D)
+
+#define ADXL345_DEVICE_ID (0xE5)
 #define ADXL345_DATA_X0 (0x32)
 
 #define ADXL345_DATA_FORMAT (0x31)
 #define ADXL345_DATA_FORMAT_RANGE_BITS (0x3 << 0)
 
-ERROR_STATUS adxl345_read_data(int16_t *x, int16_t *y, int16_t *z);
+#define ADXL345_BUFFER_LEN (128)
+
+#define ADXL345_READ_INTERVAL (0.01F) // 10 ms (100 Hz)
+
+/**
+ * A struct that contains acceleration data read from ADXL345
+ */
+typedef struct __attribute__((packed))
+{
+    float x;
+    float y;
+    float z;
+} adxl345_data_t;
+
+/**
+ * Reads data from ADXL345 sensor at given address
+ *
+ * @param address I2C address of the sensor
+ * @param data output data buffer
+ *
+ * @returns status of the sensor
+ */
+status_t adxl345_read_data(adxl345_data_t *data);
 
 #endif // IREE_RUNTIME_UTILS_ADXL345_H_
