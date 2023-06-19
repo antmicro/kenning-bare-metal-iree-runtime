27c2e1f2d59d59cd519b280f3f681dfe01dbb375
Maciej Torhan <mtorhan@antmicro.com>
2023-06-19 12:52:13 +0200

    [#45270] build_tools: configure_cmake: Add default path variables values
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- build_tools/configure_cmake.sh
+++ build_tools/configure_cmake.sh
@@ -6,10 +6,29 @@
 
 ROOT_DIR=$(dirname $(dirname $(realpath $0)))
 
+if [[ -z "${IREE_HOST_BIN_DIR=}" ]]; then
+    IREE_HOST_BIN_DIR="${ROOT_DIR?}/build/iree_compiler/bin" 
+fi
+
+if [[ -z "${RISCV_TOOLCHAIN_ROOT=}" ]]; then
+    RISCV_TOOLCHAIN_ROOT="${ROOT_DIR?}/build/toolchain_iree_rv32imf"
+fi
+
+if [[ -z "${RENODE_PATH=}" ]]; then
+    RENODE_PATH="${ROOT_DIR?}/build/renode"
+fi
+
+if [[ $@ == *"-DMINIMAL_BUILD=True"* ]]; then
+    CMAKE_TOOLCHAIN_FILE=""
+else
+    CMAKE_TOOLCHAIN_FILE="${ROOT_DIR?}/third-party/iree-rv32-springbok/cmake/riscv_iree.cmake"
+fi
+
 cmake \
     -B build/build-riscv \
-    -DCMAKE_TOOLCHAIN_FILE="${ROOT_DIR?}/third-party/iree-rv32-springbok/cmake/riscv_iree.cmake" \
+    -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TOOLCHAIN_FILE}" \
     -DCMAKE_BUILD_TYPE=MinSizeRel \
-    -DIREE_HOST_BIN_DIR="${ROOT_DIR?}/build/iree_compiler/bin" \
-    -DRISCV_TOOLCHAIN_ROOT="${ROOT_DIR?}/build/toolchain_iree_rv32imf" \
+    -DIREE_HOST_BIN_DIR="${IREE_HOST_BIN_DIR}" \
+    -DRISCV_TOOLCHAIN_ROOT="${RISCV_TOOLCHAIN_ROOT}" \
+    -DRENODE_PATH="${RENODE_PATH}" \
     $@
